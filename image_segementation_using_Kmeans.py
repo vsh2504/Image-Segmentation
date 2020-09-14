d99a38f197e8e6bf2564aae813284203e45305dd
vsh2504 <vsg13feb01@gmail.com>
2020-09-14 17:32:42 +0530

    Added code for Kmeans segmentation of image

---
+++ image_segementation_using_Kmeans.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 17:18:14 2020
+
+@author: vinayak
+"""
+
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+
+img_ = cv2.imread('image.jpg') 
+img_ = cv2.cvtColor(img_,cv2.COLOR_BGR2RGB)
+original_shape = img_.shape
+print(img_.shape)
+
+plt.imshow(img_) # as RGB Format
+plt.show()
+
+# Flattening of image
+all_pixels = img_.reshape((-1,3))
+print(all_pixels.shape)
+
+from sklearn.cluster import KMeans
+dominant_colors = 4
+km = KMeans(n_clusters=dominant_colors)
+km.fit(all_pixels)
+
+centers = km.cluster_centers_
+centers = np.array(centers,dtype='uint8')
+
+print(centers)
+
+i = 1
+
+plt.figure(0,figsize=(8,2))
+
+
+colors = []
+
+for each_col in centers:
+    plt.subplot(1,4,i)
+    plt.axis("off")
+    i+=1
+    
+    colors.append(each_col)
+    
+    a = np.zeros((100,100,3),dtype='uint8')
+    a[:,:,:] = each_col
+    plt.imshow(a)
+    
+plt.show()
+
+new_img_generated = np.zeros(all_pixels.shape,dtype="uint8")
+print(new_img_generated.shape)
+
+for i in range(new_img_generated.shape[0]):
+    new_img_generated[i] = colors[km.labels_[i]]
+
+new_img_generated = new_img_generated.reshape((original_shape))
+plt.imshow(new_img_generated)
+plt.show()
+
